dd672435c7fae442e805b10f6ac7a5091a3fed0a
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2024-10-14 22:01:30 +0200

    Prepared team booth

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TeamSelectionView from '../views/TeamSelectionView.vue'
 import PhotoboothView from '../views/PhotoboothView.vue'
+import TeamboothView from '../views/TeamboothView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/photobooth',
       name: 'photobooth',
       component: PhotoboothView
+    },
+    {
+      path: '/teambooth',
+      name: 'teambooth',
+      component: TeamboothView
     }
   ]
 });
---
+++ src/views/TeamboothView.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        Ploegfoto cut-out in 4x3 formaat
+    </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    body, html {
+        margin: 0;
+        padding: 0;
+        background-color: #e9e9e9;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+
+    main {
+        width: 100%;
+        height: 100%;
+        color: white;
+    }
+</style>
